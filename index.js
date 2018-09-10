12769cf02303cfb266639eb69e0970c5f957be3c
Daniel Parker <dparker.tech@gmail.com>
2018-09-10 08:45:46 -0500

    Init

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello world');
+});
+
+const port = process.env === 'production' ? 80 : 3000;
+
+app.listen(port, () => console.log('Listening on port:', port));
