d8a57547b67ed59f03a9ba7854493418cf3b2164
Daniel Parker <dparker.tech@gmail.com>
2018-09-10 08:51:07 -0500

    FIX: env

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ app.get('/', (req, res) => {
     res.send('Hello world');
 });
 
-const port = process.env === 'production' ? 80 : 3000;
+const port = process.env['NODE_ENV'] === 'production' ? 80 : 3000;
 
 app.listen(port, () => console.log('Listening on port:', port));
